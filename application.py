c6cf31526fe09d4743d9c839dfdaa3832d4d64aa
Masahiro Dodare <dodare@dodare0.matlab.nitech.ac.jp>
2019-12-08 15:20:49 +0900

    signin check page

--- application.py
+++ application.py
@@ -10,3 +10,11 @@ def index():
     indexhtml = render_template('index.html')
     return indexhtml
     # return "hello"
+
+@app.route("/signin-check")
+def signin_check():
+    signinhtml = render_template('signin.html')
+    return signinhtml
+
+if __name__ == '__main__':
+    app.run(debug=True)
