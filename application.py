09ffe3b6d20da521ab0f97ac1fe242b0d63a1394
Masahiro Dodare <dodare@dodare0.matlab.nitech.ac.jp>
2019-12-08 14:48:48 +0900

    frontend test

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-file = open("index.html", "r")
-indexhtml = file.read()
-file.close()
+# file = open("index.html", "r")
+# indexhtml = file.read()
+# file.close()
 
 @app.route("/")
 def index():
+    indexhtml = render_template('index.html')
     return indexhtml
     # return "hello"
