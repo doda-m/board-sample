2304e3da513854affc7cf12ad7df6373da8a0155
Masahiro Dodare <dodare@dodare0.matlab.nitech.ac.jp>
2019-12-09 12:55:31 +0900

    add sign.py

--- application.py
+++ application.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+import pyodbc
+server = 'dodare-db.database.windows.net'
+database = 'dodare-board-db'
+username = 'dodare'
+password = 'SDN@nitech'
+driver= '{ODBC Driver 13 for SQL Server}'
+
+cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+
 # file = open("index.html", "r")
 # indexhtml = file.read()
 # file.close()
@@ -9,9 +19,8 @@ app = Flask(__name__)
 def index():
     indexhtml = render_template('index.html')
     return indexhtml
-    # return "hello"
 
-@app.route("/signin-check")
+@app.check("/signin-check")
 def signin_check():
     signinhtml = render_template('signin.html')
     return signinhtml
