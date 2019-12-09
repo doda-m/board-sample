b45cb18399cd045a0859923ac0fc7f4a6b590885
Masahiro Dodare <dodare@dodare0.matlab.nitech.ac.jp>
2019-12-09 13:08:36 +0900

    fix if sentence

--- application.py
+++ application.py
@@ -14,7 +14,7 @@ cursor = cnxn.cursor()
 @app.route("/", methods=['GET', 'POST'])
 def form():
     if request.method == 'POST':
-        if str(request.form['user'] == "dodare" && str(request.form['password']) == "sdn"):
+        if str(request.form['user'] == "dodare" and str(request.form['password']) == "sdn"):
             return render_template('signin.html')
         else:
             return render_template('index.html')
