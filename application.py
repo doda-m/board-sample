82ad1c40ce5ba16438ce8341663e8015f0c2923f
Masahiro Dodare <dodare@dodare0.matlab.nitech.ac.jp>
2019-12-09 13:30:11 +0900

    test

--- application.py
+++ application.py
@@ -3,18 +3,19 @@ app = Flask(__name__)
 
 import pyodbc
 server = 'dodare-db.database.windows.net'
-database = 'dodare-board-db'
+database = 'signin'
 username = 'dodare'
 password = 'SDN@nitech'
-driver= '{ODBC Driver 13 for SQL Server}'
+driver = '{ODBC Driver 13 for SQL Server}'
 
 # cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 # cursor = cnxn.cursor()
 
 # @app.route("/", methods=['GET', 'POST'])
 @app.route("/")
-def form():
-    return render_template('index.html')
+def indexform():
+    indexhtml = render_template('index.html')
+    return indexhtml
     # if request.method == 'POST':
     #     if str(request.form['user'] == "dodare" and str(request.form['password']) == "sdn"):
     #         return render_template('signin.html')
