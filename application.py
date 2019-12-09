f7f55410e9e69693bd331aa6f3559a688a181774
Masahiro Dodare <dodare@dodare0.matlab.nitech.ac.jp>
2019-12-09 12:59:59 +0900

    integrate to app.py

--- application.py
+++ application.py
@@ -11,14 +11,18 @@ driver= '{ODBC Driver 13 for SQL Server}'
 cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
-# file = open("index.html", "r")
-# indexhtml = file.read()
-# file.close()
-
-@app.route("/")
-def index():
-    indexhtml = render_template('index.html')
-    return indexhtml
+@app.route("/", methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        if(str(request.form['user']) == "dodare"
+        && str(request.form['password']) == "sdn") {
+            return render_template('signin.html')
+        }
+        else {
+            return render_template('index.html')
+        }
+    else:
+        return render_template('index.html')
 
 @app.check("/signin-check")
 def signin_check():
